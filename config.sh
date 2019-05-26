88122eb4ad8a6b4d4770520a679b071ab23aeaa8
fabiottini <fabiottini@gmail.com>
2019-05-26 20:52:30 +0200

    WIP: init refactoring

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+##################### config parameter ####################
+DEBUG_LEVEL=2
+SILENT=1
+CHECKSUM="md5 -r" ## fix for mac 
+
+##################### usefull parameter ###################
+MISSING_FILES=0
+DIFFERENT_FILES=0
+ERRORS=0
