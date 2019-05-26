3346516b1e08034dc57fb0b74be35610344e4d6b
Matteo Mazza <matteom.mazza@gmail.com>
2019-05-26 03:01:35 +0100

    test: fix test permission

--- test/sh/equal-files-in-src-and-dst-must-success.sh
+++ test/sh/equal-files-in-src-and-dst-must-success.sh
@@ -16,6 +16,7 @@ echo "yet another test" > dst/subdir/file1
 echo "yet another line" >> src/subdir/file1
 echo "yet another line" >> dst/subdir/file1
 
+chmod +x ./checksum-comparator.sh
 ./checksum-comparator.sh --debug src dst
 if [ $? = 0 ]; then
   exit 0
