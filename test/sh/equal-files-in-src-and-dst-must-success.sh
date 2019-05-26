18d9fe695afbd3043502ba16dce7f25c98beb1d8
Matteo Mazza <matteom.mazza@gmail.com>
2019-05-26 03:21:02 +0100

    test: add a testing pipeline (#7)

---
+++ test/sh/equal-files-in-src-and-dst-must-success.sh
@@ -0,0 +1,25 @@
+mkdir src
+mkdir dst
+
+mkdir src/subdir
+mkdir dst/subdir
+
+touch src/file1
+touch dst/file1
+
+echo "test" > src/file2
+echo "test" > dst/file2
+
+
+echo "yet another test" > src/subdir/file1
+echo "yet another test" > dst/subdir/file1
+echo "yet another line" >> src/subdir/file1
+echo "yet another line" >> dst/subdir/file1
+
+chmod +x ./checksum-comparator.sh
+bash ./checksum-comparator.sh --debug src dst
+if [ $? = 0 ]; then
+  exit 0
+fi
+
+exit 1
