e3e797fb83203e1a2e6a08d33d894abeffc1d6d1
Matteo Mazza <matteom.mazza@gmail.com>
2019-05-26 04:24:40 +0100

    test: fix broken test

--- test/sh/update-file-in-src-or-dst-dir-after-checksum-computed-must-fail.sh
+++ test/sh/update-file-in-src-or-dst-dir-after-checksum-computed-must-fail.sh
@@ -22,15 +22,20 @@ echo "yet another line" >> dst/subdir/file1
 chmod +x ./checksum-comparator.sh
 bash ./checksum-comparator.sh --debug src dst
 
+# simulate the user doing something
+# note: depending on the precision of the stored modify date, 
+#       this test can fail without this sleep command because too fast during execution phase
+sleep 1
 
 echo "one update" > dst/subdir/file1
 echo ""
-ls -lah dst/subdir/ # shows date of files
+stat dst/subdir/file1* 
 
 # it must detect that an update is occurred and must recompute the checksum
 bash ./checksum-comparator.sh --debug src dst
 if [ $? = 0 ]; then
-  ls -lah dst/subdir/
+  echo ""
+  stat dst/subdir/file1*
   exit 1
 fi
 
